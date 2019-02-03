d468a1ee586af617f081f86cd87edbe98c223275
Rajeev Ranjan <rajeev2.ranjan@intel.com>
2019-02-03 17:16:18 +0530

    Check for empty property list in TP
    1.)Add check for empty property list if proposal for reporter
    
    Signed-off-by: Rajeev Ranjan <rajeev2.ranjan@intel.com>

--- processor/src/handler.rs
+++ processor/src/handler.rs
@@ -1083,6 +1083,11 @@ impl SupplyChainTransactionHandler {
                     "Only the owner can create a proposal to change ownership",
                 )));
             }
+            if role == proposal::Proposal_Role::REPORTER && properties.len() == 0 {
+                return Err(ApplyError::InvalidTransaction(String::from(
+                    "Property list cannot be empty for Reporter role",
+                )))
+            }
         }
 
         if role == proposal::Proposal_Role::CUSTODIAN {
